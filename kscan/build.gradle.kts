afa2caaf14614f0d7e08ec34c366d4ec23e2ad11
ismail.mohamed <ismailncgroup@gmail.com>
2025-11-01 08:44:25 +0000

    bump version

--- kscan/build.gradle.kts
+++ kscan/build.gradle.kts
@@ -69,7 +69,7 @@ mavenPublishing {
     // or when publishing to https://s01.oss.sonatype.org
     publishToMavenCentral()
     signAllPublications()
-    coordinates("io.github.ismai117", "KScan", "0.3.2")
+    coordinates("io.github.ismai117", "KScan", "0.4.0")
 
     pom {
         name.set(project.name)
